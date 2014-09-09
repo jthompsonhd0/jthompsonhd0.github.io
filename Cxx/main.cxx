5a9e1c1f9c3f1b44e114deef9091e33f4a13442e
jthompsonhd0 <jthompsonhd0@gmail.com>
2014-09-09 16:42:43 -0700

    Changed main.cxx

--- Cxx/main.cxx
+++ Cxx/main.cxx
@@ -1,10 +1,17 @@
 #include <iostream>
+#include <math.h>
 #include "header.h"
+
+using namespace std;
 
 int main(){
 	
 	myHelloFunction();
 	
+	double x = 1.2;
+	double y = 3.14159265359;
+	
+	cout << "x = " << x << " and y = " << y << endl;
 	
 	return 0;
 }
